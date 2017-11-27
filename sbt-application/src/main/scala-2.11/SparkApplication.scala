b243fc1e65529a1a2c39f99bdbda2106227ad061
Benny Halberstadt <benny.halberstadt@weather.com>
2017-11-27 14:00:21 +0200

    added param fro num of records

--- sbt-application/src/main/scala-2.11/SparkApplication.scala
+++ sbt-application/src/main/scala-2.11/SparkApplication.scala
@@ -15,7 +15,8 @@ object SparkApplication {
       .builder
       .appName("Amazon Reviews")
       .getOrCreate()
-
+	  
+	val numRecords = args(0).toInt;
     spark.sparkContext.setLogLevel("ERROR")
     import spark.implicits._
 
@@ -24,15 +25,15 @@ object SparkApplication {
     val ds = spark.read.format("csv").option("header", "true").load(reviews);
     ds.createOrReplaceTempView("Review")
 
-    spark.sql("select UserId, count(*) as count from Review group by UserId order by count desc").limit(20).show();
+    spark.sql("select UserId, count(*) as count from Review group by UserId order by count desc").limit(numRecords).show();
    
-    spark.sql("select ProductId, count(*) as count from Review group by ProductId order by count desc").limit(20).show();
+    spark.sql("select ProductId, count(*) as count from Review group by ProductId order by count desc").limit(numRecords).show();
 
     
     val dsWords = ds.withColumn("Text", lower(regexp_replace($"Text", "(!|\\)|\\(|,|\"|\\.)", ""))).withColumn("Text", split($"Text", "\\s+")).select(explode($"Text"));
     dsWords.createOrReplaceTempView("Words")
 
-    spark.sql("select col as word, count(*) as count from Words group by col order by count desc").limit(20).show();
+    spark.sql("select col as word, count(*) as count from Words group by col order by count desc").limit(numRecords).show();
 
     spark.stop()
   }
